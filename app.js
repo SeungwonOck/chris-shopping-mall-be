40a68bebdfabafdd26d6b2faa3b832793aaab9cd
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-05-31 00:37:09 +1000

    Fixed typo

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json()) // req.body가 객체로 인식이 된다.
 
-const mongoURI = process.env.LOCAL_DB_ADDREESS;
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connect(mongoURI)
     .then(() => console.log("mongoose connected"))
     .catch((err) => console.log("DB Connection fail", err));
 
