f79816be629271904047202c0957a27096d15fef
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-02 01:33:55 +1000

    Login Function

---
+++ controllers/auth.controller.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require("bcryptjs");
+const authController = {}
+
+authController.loginWithEmail = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        let user = await User.findOne({ email })
+        if (user) {
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                //token
+                const token = await user.generateToken();
+                return res.status(200).json({status: "success", user, token})
+            }
+        }
+        throw new Error("Invalid email or password")
+    } catch (error) {
+        res.status(400).json({ status: "error", error: error.message });
+    }
+}
+
+module.exports = authController;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,8 +15,8 @@ userController.createUser = async (req, res) => {
         const newUser = new User({ email, password, name, level: level ? level : 'customer' });
         await newUser.save();
         return res.status(200).json({status: "success"})
-    } catch (err) {
-        res.status(400).json({status: "fail", err: err.message})
+    } catch (error) {
+        res.status(400).json({status: "fail", error: error.message})
     }
 }
 
