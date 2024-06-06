611b48a63b34ecfd7b1c0ddab8773b576420766b
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-07 01:10:34 +1000

    Product Search and PageNation

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require('../models/Product');
+const PAGE_SIZE = 1;
 
 const productController = {}
 
@@ -25,8 +26,20 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
-        res.status(200).json({ status: "success", data: products })
+        const { page, name } = req.query;
+        const cond = name ? { name: { $regex: name, $options: "i" } } : {}
+        let query = Product.findOne(cond)
+        let response = { status: "success" }
+        if (page) {
+            query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+            //최종 몇개 페이지, 데이터가 총 몇개있는지
+            const totalItemNum = await Product.find(cond).count();
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.totalPageNum = totalPageNum;
+        }
+        const productList = await query.exec()
+        response.data = productList
+        res.status(200).json(response)
     } catch (error) {
         res.status(400).json({ status: "fail", error: error.message })
     }
