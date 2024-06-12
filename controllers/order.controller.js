197b0d8918c64629f57c3c1b79492ba376295cf5
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-13 00:33:16 +1000

    AdminOrderPage with Search Function and Pagination

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -57,4 +57,34 @@ orderController.getOrder = async (req, res) => {
     }
 }
 
+orderController.getOrderList = async (req, res) => {
+    try {
+        const { page, orderNum } = req.query;
+
+        let cond = {};
+        if (orderNum) {
+            cond = {
+                orderNum: { $regex: orderNum, $options: "i" }
+            };
+        }
+
+        const orderList = await Order.find(cond)
+            .populate("userId")
+            .populate({
+                path: "items",
+                populate: {
+                    path: "productId",
+                    model: "Product",
+                    select: "image name",
+                },
+            }).skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+        
+        const totalItemNum = await Order.find(cond).count();
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+        res.status(200).json({status: "success", data: orderList, totalPageNum})
+    } catch (error) {
+        res.status(400).json({status: "fail", error: error.message})
+    }
+}
+
 module.exports = orderController;
--- routes/order.api.js
+++ routes/order.api.js
@@ -5,6 +5,7 @@ const orderController = require("../controllers/order.controller");
 const authController = require('../controllers/auth.controller');
 
 router.get("/me", authController.authenticate, orderController.getOrder)
+router.get("/", authController.authenticate, orderController.getOrderList)
 
 router.post("/", authController.authenticate, orderController.createOrder)
 
